38febd92e583cd51a6878d5348c4ba5412891153
changlie <583491331@qq.com>
2020-08-08 18:50:16 +0800

    update.

--- src/core/505module_class_executor.go
+++ src/core/505module_class_executor.go
@@ -2,9 +2,16 @@ package core
 
 import "reflect"
 
+// 类定义
 type ClassDefine struct {
 	name string // class name
 	fields map[string]*reflect.Type
 	methods map[string]*FunctionExecutor
+}
+
+// 类对象
+type ClassExecutor struct {
+	define *ClassDefine
+	fields map[string]*reflect.Value
 }
 
