6954cd4a2ccc6b40f42672483ba94eadb7bd22a8
changlie <583491331@qq.com>
2020-08-31 23:17:33 +0800

    update

--- src/core/505module_class_executor.go
+++ src/core/505module_class_executor.go
@@ -5,26 +5,21 @@ import (
 	"reflect"
 )
 
-// 类定义
-type ClassDefine struct {
-	name    string // class name
-	fields  map[string]reflect.Type
-	methods map[string]*FunctionExecutor
-}
 
 // 类对象
 type ClassExecutor struct {
-	define *ClassDefine
-	fields map[string]*reflect.Value
+	name    string // class name
+	fields  map[string]*FieldInfo
+	methods map[string]*FunctionExecutor
 }
 
 func (clazz *ClassExecutor) fieldValue(name string) interface{} {
 	val, exist := clazz.fields[name]
-	assert(!exist, fmt.Sprintf("%v.%v is undefined!", clazz.define.name, name))
-	return val.Interface()
+	assert(!exist, fmt.Sprintf("%v.%v is undefined!", clazz.name, name))
+	return val.v.Interface()
 }
 
 func (clazz *ClassExecutor) setField(name string, rawVal interface{}) {
 	goVal := reflect.ValueOf(rawVal)
-	clazz.fields[name] = &goVal
+	clazz.fields[name].v = goVal
 }
